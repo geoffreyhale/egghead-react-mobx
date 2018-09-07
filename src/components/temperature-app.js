692b0683cf878eeb71d373222c942800f4c98fad
Geoffrey Hale <geoffrey.hale@appfolio.com>
2018-09-07 14:00:29 -0700

    observable temperatures Map

--- src/components/temperature-app.js
+++ src/components/temperature-app.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import {observable} from 'mobx';
 import {observer} from 'mobx-react';
 import DevTools from 'mobx-react-devtools';
 
+import Temperature from './temperature';
+
+window.Temperature = Temperature;
+window.temps = observable.map({
+  "Amsterdam": new Temperature(),
+  "Rome": new Temperature()
+});
+//console: temps.set("Tel Aviv", new Temperature());
+
 const TemperatureApp = observer(({ temperatures }) => (
   <div>
-    {temperatures.map(t =>
-      <div key={t.id}>{t.temperature}</div>
+    {Array.from(temperatures).map(([k,t]) =>
+      <div key={k}>{t.temperature}</div>
     )}
     <DevTools />
   </div>
 ));
 
-export default TemperatureApp;
+export default function() {
+  return <TemperatureApp temperatures={window.temps} />
+};
--- src/index.js
+++ src/index.js
@@ -1,20 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {observable} from 'mobx';
 
 import Counter from './components/counter';
-import Temperature from './components/temperature';
 import TemperatureApp from './components/temperature-app';
-
-window.temps = observable([]);
-temps.push(new Temperature());
-temps.push(new Temperature());
 
 const Index = () => {
   return (
     <div>
       <Counter />
-      <TemperatureApp temperatures={window.temps} />
+      <TemperatureApp />
     </div>
   )
 };
