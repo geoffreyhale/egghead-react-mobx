8d9c5f424ae1f633633ee18444b9b915c69fb29b
Geoffrey Hale <geoffrey.hale@appfolio.com>
2018-09-07 12:31:55 -0700

    add appState store

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,26 +1,24 @@
 import React, { Component } from 'react';
-import { observable } from 'mobx';
 import { observer } from 'mobx-react';
+import appState from '../stores/app';
 
 @observer
 export default class Counter extends Component {
-  @observable count = 0;
-
   render() {
     return (
       <div>
-        Counter: {this.count} <br/>
+        Counter: {appState.count} <br/>
         <button onClick={this.handleInc.bind(this)}> + </button>
         <button onClick={this.handleDec.bind(this)}> - </button>
       </div>
     )
   }
 
-  handleInc() {
-    this.count++;
-  };
+  handleInc = () => {
+    appState.increment();
+  }
 
-  handleDec() {
-    this.count--;
+  handleDec = () => {
+    appState.decrement();
   }
 }
