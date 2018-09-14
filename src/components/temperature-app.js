04e78a2d6ae04df17e1920982f3e8a8a3a66197b
Geoffrey Hale <geoffrey.hale@appfolio.com>
2018-09-14 11:26:26 -0700

    pass observable data through props

--- src/components/temperature-app.js
+++ src/components/temperature-app.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import {observable} from 'mobx';
-import {observer} from 'mobx-react';
-import DevTools from 'mobx-react-devtools';
+import React from "react";
+import { action, observable } from "mobx";
+import { observer } from "mobx-react";
+import DevTools from "mobx-react-devtools";
 
-import Temperature from './temperature';
+import Temperature from "./temperature";
 
 window.Temperature = Temperature;
-window.temps = observable.map({
-  "Amsterdam": new Temperature(),
-  "Rome": new Temperature()
-});
+// window.temps = observable.map({
+//   "Amsterdam": new Temperature(),
+//   "Rome": new Temperature()
+// });
 //console: temps.set("Tel Aviv", new Temperature());
+window.temps = [];
+temps.push(new Temperature(20, "K"));
+temps.push(new Temperature(25, "F"));
+temps.push(new Temperature(20, "C"));
 
 const TemperatureApp = observer(({ temperatures }) => (
-  <div>
-    {Array.from(temperatures).map(([k,t]) =>
-      <div key={k}>{t.temperature}</div>
-    )}
+  <ul>
+    {temperatures.map(t => (
+      <TView key={t.id} temperature={t} />
+    ))}
     <DevTools />
-  </div>
+  </ul>
 ));
 
+@observer
+class TView extends React.Component {
+  render() {
+    const t = this.props.temperature;
+    return <li onClick={this.onTemperatureClick}>{t.temperature}</li>;
+  }
+
+  @action
+  onTemperatureClick = () => {
+    this.props.temperature.inc();
+  };
+}
+
 export default function() {
-  return <TemperatureApp temperatures={window.temps} />
-};
+  return <TemperatureApp temperatures={window.temps} />;
+}
--- src/components/temperature.js
+++ src/components/temperature.js
@@ -1,43 +1,63 @@
-import React from 'react';
-import {action, computed, configure, observable} from 'mobx';
-import {observer} from 'mobx-react';
-import {Component} from 'react';
+import React from "react";
+import { action, computed, configure, extendObservable } from "mobx";
+import { observer } from "mobx-react";
+import { observable } from "mobx";
+import { Component } from "react";
+import { IncomingMessage } from "http";
 
 configure({
-  enforceActions: 'observed'
+  enforceActions: "observed"
 });
 
 export default class Temperature {
-  @observable unit = "C";
-  @observable temperatureCelsius = 25;
+  constructor(temperatureCelsius, unit) {
+    extendObservable;
+  }
+  @observable
+  unit = "C";
+  @observable
+  temperatureCelsius = 25;
 
-  @computed get temperatureFahrenheit() {
-    return this.temperatureCelsius * (9/5) + 32
+  @computed
+  get temperatureFahrenheit() {
+    return this.temperatureCelsius * (9 / 5) + 32;
   }
 
-  @computed get temperatureKelvin() {
+  @computed
+  get temperatureKelvin() {
     return this.temperatureCelsius + 273.15;
   }
 
-  @computed get temperature() {
-    switch(this.unit) {
-      case "K": return this.temperatureKelvin + "*K";
-      case "F": return this.temperatureFahrenheit + "*F";
-      case "C": return this.temperatureCelsius + "*C";
+  @computed
+  get temperature() {
+    switch (this.unit) {
+      case "K":
+        return this.temperatureKelvin + "*K";
+      case "F":
+        return this.temperatureFahrenheit + "*F";
+      case "C":
+        return this.temperatureCelsius + "*C";
     }
   }
 
-  @action setUnit(newUnit) {
+  @action
+  setUnit(newUnit) {
     this.unit = newUnit;
   }
 
-  @action setCelsius(degrees) {
+  @action
+  setCelsius(degrees) {
     this.temperatureCelsius = degrees;
   }
 
-  @action("update temperature and unit")
+  @action
   setTemperatureAndUnit(degrees, unit) {
     this.setCelsius(degrees);
     this.setUnit(unit);
   }
-};
+
+  @action
+  inc() {
+    this.setCelsius(this.temperatureCelsius + 1);
+  }
+}
