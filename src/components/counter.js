bb50faaa3d33c387702013242b8a5e4216e35a29
Geoffrey Hale <geoffreyhale@gmail.com>
2018-09-21 11:59:44 -0700

    fix counter app

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,24 +1,27 @@
-import React, { Component } from 'react';
-import { observer } from 'mobx-react';
-import appState from '../stores/app';
+import React, { Component } from "react";
+import { action } from "mobx";
+import { observer } from "mobx-react";
+import appState from "../stores/app";
 
 @observer
 export default class Counter extends Component {
   render() {
     return (
       <div>
-        Counter: {appState.count} <br/>
+        Counter: {appState.count} <br />
         <button onClick={this.handleInc}> + </button>
         <button onClick={this.handleDec}> - </button>
       </div>
-    )
+    );
   }
 
+  @action
   handleInc = () => {
     appState.increment();
-  }
+  };
 
+  @action
   handleDec = () => {
     appState.decrement();
-  }
+  };
 }
